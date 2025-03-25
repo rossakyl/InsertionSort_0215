f71bf7ba297c1172eb0cdecc8c7bde2bb7f89ad9
rossakyl <rossakayla01@gmail.com>
2025-03-25 12:32:32 +0800

    ensure array size not exceed 20

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -10,5 +10,20 @@ while (true)
 {
 cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan Jumlah element Array
 cin >> n; // Memanggil variabel inputan n
+
+if (n <= 20)
+{ // Membuat kondisi n tidak lebih dari 20
+break;
 }
+else
+{
+cout << "n\Array yang anda masukkan maksimal 20 Element.\n"; // Menampilkan pesan jika data lebih dari 20
+}
+}
+cout << endl; // Membuat jarak per baris program
+cout << "==================" << endl; // Membuat tampilan susunan data element array
+cout << "Masukkan Elemnt Array" << endl;
+cout << "==================" << endl;
+
+
 }
