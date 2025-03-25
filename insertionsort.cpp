fa669e3441d6658c247a3216692537612eb7f2bf
rossakyl <rossakayla01@gmail.com>
2025-03-25 12:48:02 +0800

    format output for sorted array display

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -54,3 +54,14 @@ j--; // Step 4b
 arr[j + 1] = temp; // Step 5
 }
 }
+
+void display()
+{
+cout << endl; // Output baris kosong
+cout << "====================" << endl;
+cout << "Total passs = " << n - 1 << endl; // count element movement  
+cout << "====================" << endl; // Output ke layar
+cout << "Element Array yang telah tersusun" << endl; // Output ke layar
+cout << "====================" << endl; // Output ke layar
+
+}
