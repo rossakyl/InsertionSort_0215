656ae0af1b1416a05af0d21e4290e156e7b86523
rossakyl <rossakayla01@gmail.com>
2025-03-25 12:25:49 +0800

    create procedure to input array size

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]; //Membuat Array dengan panjang data 20
-int n; //Membuat Variabel inputan n
+int arr[20]; // Membuat Array dengan panjang data 20
+int n; // Membuat Variabel inputan n
+
+void input()
+{ // Procedure Input
+while (true)
+{
+cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan Jumlah element Array
+cin >> n; // Memanggil variabel inputan n
+}
+}
